7839abc55ab47233dc63c0e19eef06b31713ac43
Basic Account <fuad.aliyev.std.@bhos.edu.az>
2024-12-02 18:27:44 +0400

    latest

--- main.c
+++ main.c
@@ -2,23 +2,21 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
-#include<ctype.h>
-#ifdef _WIN32
-    #include <conio.h>
-    #include <windows.h>
-    SetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), ENABLE_VIRTUAL_TERMINAL_PROCESSING);
-#endif
+#include <ctype.h>
 #define WIDTH 10
 #define HEIGHT 20
 
-#define RED '#'
-#define GREEN '#'
-#define BLUE '#'
-#define YELLOW '#'
+#define DEFAULT "#"
+#define RED "\033[31m#\033[0m"
+#define GREEN "\033[32m#\033[0m"
+#define YELLOW "\033[33m#\033[0m"
+#define BLUE "\033[34m#\033[0m"
+#define MAGENTA "\033[35m#\033[0m"
+#define CYAN "\033[36m#\033[0m"
 
-char blockColors[] = {RED, GREEN, BLUE, YELLOW};
+char *blockColors[] = {RED, GREEN, BLUE, YELLOW, BLUE, MAGENTA, CYAN};
 
-char grid[HEIGHT][WIDTH];
+char *grid[HEIGHT][WIDTH];
 int shapes[10][4][4] = {
     {{0, 1, 0, 0}, {0, 1, 0, 0}, {0, 1, 0, 0}, {0, 1, 0, 0}}, 
     {{1, 1, 0, 0}, {0, 1, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}}, 
@@ -56,9 +54,6 @@ struct coord{
 };
 
 void displayboard() {
-    #ifdef _WIN32
-    system("cls");
-    #endif
     printf("\033[2J\033[H");
     printf("        TETRIS\t\t\tPoints: %d\n ",totalpoints);
     
@@ -68,7 +63,7 @@ void displayboard() {
     for (int i = 0; i < HEIGHT; i++) {
         printf("|"); 
         for (int j = 0; j < WIDTH; j++) {
-            printf("%c ", grid[i][j]); 
+            printf("%s ", grid[i][j]); 
         }
         printf("|\n ");
     }
@@ -78,11 +73,11 @@ void displayboard() {
 }
 
 int placeshapes(int type) {
-    char color = blockColors[type % 4];
+    char *color = blockColors[type % 4];
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
             if (shapes[type][i][j] == 1) {
-                if(grid[i][j+3]=='#'){
+                if(grid[i][j+3]==DEFAULT){
                     displayboard();
                     return -1;
                 }
@@ -98,7 +93,7 @@ int placeshapes(int type) {
 void initboard() {
     for (int i = 0; i < HEIGHT; i++) {
         for (int j = 0; j < WIDTH; j++) {
-            grid[i][j] = ' '; 
+            grid[i][j] = " "; 
         }
     }
 }
@@ -111,7 +106,7 @@ int movedown(struct coord* lastone){
     for(int i=0;i<4;i++){
         for(int j=0;j<4;j++){
             if(lastone->shapeofit[i][j]==1 && (lastone->shapeofit[i+1][j]!=1 || i+1>=4)){
-                if(grid[lastone->Y+i+1][lastone->X+j]=='#') return -1;
+                if(grid[lastone->Y+i+1][lastone->X+j]!=" ") return -1;
             }
         }
     }
@@ -119,9 +114,9 @@ int movedown(struct coord* lastone){
         for(int j=0;j<4;j++){
             if(lastone->shapeofit[i][j]==1){
                 if(lastone->shapeofit[i-1][j]!=1 || i-1<0){
-                    grid[lastone->Y+i][lastone->X+j]=' ';
+                    grid[lastone->Y+i][lastone->X+j]=" ";
                 }
-                grid[lastone->Y+i+1][lastone->X+j]=blockColors[lastone->type % 4];
+                grid[lastone->Y+i+1][lastone->X+j]=blockColors[lastone->type % 7];
             }
         }
     }
@@ -135,7 +130,7 @@ int moveleft(struct coord* lastone){
     for(int i=0;i<4;i++){
         for(int j=0;j<4;j++){
             if(lastone->shapeofit[i][j]==1){
-                if(grid[lastone->Y+i][lastone->X+j-1]=='#') return -1;
+                if(grid[lastone->Y+i][lastone->X+j-1]!=" ") return -1;
                 else break;
             }
         }
@@ -144,13 +139,14 @@ int moveleft(struct coord* lastone){
         for(int j=0;j<4;j++){
             if(lastone->shapeofit[i][j]==1){
                 if(lastone->shapeofit[i][j+1]!=1 || j+1>WIDTH){
-                    grid[lastone->Y+i][lastone->X+j]=' ';
+                    grid[lastone->Y+i][lastone->X+j]=" ";
                 }
-                grid[lastone->Y+i][lastone->X+j-1]=blockColors[lastone->type % 4];
+                grid[lastone->Y+i][lastone->X+j-1]=blockColors[lastone->type % 7];
             }
         }
     }
     lastone->X--;
+    return 0;
 }
 
 int moveright(struct coord* lastone){
@@ -159,7 +155,7 @@ int moveright(struct coord* lastone){
     for(int i=3;i>=0;i--){
         for(int j=3;j>=0;j--){
             if(lastone->shapeofit[i][j]==1){
-                if(grid[lastone->Y+i][lastone->X+j+1]=='#') return -1;
+                if(grid[lastone->Y+i][lastone->X+j+1]!=" ") return -1;
                 else break;
             }
         }
@@ -168,13 +164,14 @@ int moveright(struct coord* lastone){
         for(int j=0;j<4;j++){
             if(lastone->shapeofit[i][j]==1){
                 if(lastone->shapeofit[i][j-1]!=1 || j-1<0){
-                    grid[lastone->Y+i][lastone->X+j]=' ';
+                    grid[lastone->Y+i][lastone->X+j]=" ";
                 }
-                grid[lastone->Y+i][lastone->X+j+1]=blockColors[lastone->type % 4];
+                grid[lastone->Y+i][lastone->X+j+1]=blockColors[lastone->type % 7];
             }
         }
     }
     lastone->X++;
+    return 0;
 }
 
 void movelinesdown(int height){
@@ -192,7 +189,7 @@ void checkforlines(){
         for (int i=HEIGHT-1;i>=0;i--){
             wrong=0;
             for(int j=0;j<WIDTH;j++){
-                if(grid[i][j]!='#'){
+                if(grid[i][j]==" "){
                     wrong=1;
                     break;
                 }
@@ -211,29 +208,29 @@ void rotate(struct coord* lastone) {
     usleep(30000);
     int extra[4][4] = {0}; 
     int left=4, right=-1, down=-1;
-    for(int i = 0; i < 4; i++) {
-        for(int j = 0; j < 4; j++) {
+    for(int i=0; i<4; i++) {
+        for(int j=0; j<4; j++) {
             if(lastone->shapeofit[i][j] == 1) {
-                grid[lastone->Y + i][lastone->X + j] = ' ';
+                grid[lastone->Y+i][lastone->X+j]=" ";
             }
         }
     }
-    for(int i = 0; i < 4; i++) {
-        for(int j = 0; j < 4; j++) {
+    for(int i=0; i<4; i++) {
+        for(int j=0; j<4; j++) {
             extra[j][lastone->edges[2]-i-1] = lastone->shapeofit[i][j];
         }
     }
-    for(int i = 0; i < 4; i++) {
-        for(int j = 0; j < 4; j++) {
+    for(int i=0; i<4; i++) {
+        for(int j=0; j<4; j++) {
             if(extra[i][j] == 1) {
 
                 if(lastone->X + j >= WIDTH || lastone->X + j < 0 || 
                    lastone->Y + i >= HEIGHT || 
-                   grid[lastone->Y + i][lastone->X + j] == '#') {
+                   grid[lastone->Y + i][lastone->X + j] != " ") {
                     for(int y = 0; y < 4; y++) {
                         for(int x = 0; x < 4; x++) {
                             if(lastone->shapeofit[y][x] == 1) {
-                                grid[lastone->Y + y][lastone->X + x] = blockColors[lastone->type % 4];
+                                grid[lastone->Y + y][lastone->X + x] = blockColors[lastone->type % 7];
                             }
                         }
                     }
@@ -242,23 +239,23 @@ void rotate(struct coord* lastone) {
             }
         }
     }
-    for(int i = 0; i < 4; i++) {
-        for(int j = 0; j < 4; j++) {
-            lastone->shapeofit[i][j] = extra[i][j];
+    for(int i=0; i<4; i++) {
+        for(int j=0; j<4; j++) {
+            lastone->shapeofit[i][j]=extra[i][j];
             if(extra[i][j] == 1) {
                 if(j < left) left = j;
-                if(j+1 > right) right = j+1;
-                if(i+1 > down) down = i+1;
+                if(j+1>right) right = j+1;
+                if(i+1>down) down = i+1;
             }
         }
     }
-    lastone->edges[0] = left;
-    lastone->edges[1] = right;
-    lastone->edges[2] = down;
-    for(int i = 0; i < 4; i++) {
-        for(int j = 0; j < 4; j++) {
+    lastone->edges[0]=left;
+    lastone->edges[1]=right;
+    lastone->edges[2]=down;
+    for(int i=0; i<4; i++) {
+        for(int j=0; j<4; j++) {
             if(lastone->shapeofit[i][j] == 1) {
-                grid[lastone->Y + i][lastone->X + j] = blockColors[lastone->type % 4];
+                grid[lastone->Y+i][lastone->X+j] = blockColors[lastone->type % 7];
             }
         }
     }
@@ -316,11 +313,12 @@ int main() {
                 rotate(&lastone);
             }
             else if(current==3){ //Ctrl+C
+                restore_terminal();
                 return 0;
             }
             displayboard();
         }
-        if(lastone.Y=0){
+        if(lastone.Y==0){
             gameactive=0;
             break;
         }
