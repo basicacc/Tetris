9350b5c342b7b9dd627e8dd655cecea19369fbfe
Basic Account <fuad.aliyev.std.@bhos.edu.az>
2024-12-02 16:04:38 +0400

    rotation and few more additions

--- main.c
+++ main.c
@@ -19,59 +19,48 @@
 char blockColors[] = {RED, GREEN, BLUE, YELLOW};
 
 char grid[HEIGHT][WIDTH];
-int shapes[7][4][4] = {
+int shapes[10][4][4] = {
     {{0, 1, 0, 0}, {0, 1, 0, 0}, {0, 1, 0, 0}, {0, 1, 0, 0}}, 
     {{1, 1, 0, 0}, {0, 1, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}}, 
     {{0, 1, 1, 0}, {1, 1, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}}, 
     {{1, 1, 1, 0}, {0, 1, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}}, 
     {{1, 1, 0, 0}, {1, 1, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}}, 
     {{1, 0, 0, 0}, {1, 1, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}},
-    {{0, 0, 1, 0}, {1, 1, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}}
+    {{0, 0, 1, 0}, {1, 1, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}},
+    {{1, 1, 1, 0}, {0, 1, 0, 0}, {0, 1, 0, 0}, {0, 0, 0, 0}},
+    {{1, 1, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}},
+    {{1, 1, 0, 0}, {1, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}}
 };
 
-int shapesleftrightheight[7][3] = {
+int shapesleftrightheight[10][3] = {
     {1,2,4},
     {0,3,2},
     {0,3,2},
     {0,3,2},
     {0,2,2},
     {0,3,2},
-    {0,3,2}
+    {0,3,2},
+    {0,3,3},
+    {0,3,1},
+    {0,2,2}
 };
+
+int totalpoints=0;
 
 struct coord{
     int X;
     int Y;
     int type;
+    int shapeofit[4][4];
+    int edges[3];
 };
-
-void placeshapes(int type) {
-    char color = blockColors[type % 4];
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            if (shapes[type][i][j] == 1) {
-                if (i < HEIGHT && j < WIDTH) { 
-                    grid[i][j] = color; 
-                }
-            }
-        }
-    }
-}
-
-void initboard() {
-    for (int i = 0; i < HEIGHT; i++) {
-        for (int j = 0; j < WIDTH; j++) {
-            grid[i][j] = ' '; 
-        }
-    }
-}
 
 void displayboard() {
     #ifdef _WIN32
     system("cls");
     #endif
     printf("\033[2J\033[H");
-    printf("      TETRIS GAME\n ");
+    printf("        TETRIS\t\t\tPoints: %d\n ",totalpoints);
     
     for (int i = 0; i < WIDTH * 2 + 2; i++) printf("=");
     printf("\n ");
@@ -88,20 +77,48 @@ void displayboard() {
     printf("\n");
 }
 
+int placeshapes(int type) {
+    char color = blockColors[type % 4];
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (shapes[type][i][j] == 1) {
+                if(grid[i][j+3]=='#'){
+                    displayboard();
+                    return -1;
+                }
+                if (i < HEIGHT && j < WIDTH) { 
+                    grid[i][j+3] = color; 
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+void initboard() {
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            grid[i][j] = ' '; 
+        }
+    }
+}
+
+
+
 int movedown(struct coord* lastone){
     usleep(300000);
-    if(shapesleftrightheight[lastone->type][2]+lastone->Y==HEIGHT) return -1;
+    if(lastone->edges[2]+lastone->Y==HEIGHT) return -1;
     for(int i=0;i<4;i++){
         for(int j=0;j<4;j++){
-            if(shapes[lastone->type][i][j]==1 && shapes[lastone->type][i+1][j]!=1){
+            if(lastone->shapeofit[i][j]==1 && (lastone->shapeofit[i+1][j]!=1 || i+1>=4)){
                 if(grid[lastone->Y+i+1][lastone->X+j]=='#') return -1;
             }
         }
     }
     for(int i=0;i<4;i++){
         for(int j=0;j<4;j++){
-            if(shapes[lastone->type][i][j]==1){
-                if(shapes[lastone->type][i-1][j]!=1 || i-1<0){
+            if(lastone->shapeofit[i][j]==1){
+                if(lastone->shapeofit[i-1][j]!=1 || i-1<0){
                     grid[lastone->Y+i][lastone->X+j]=' ';
                 }
                 grid[lastone->Y+i+1][lastone->X+j]=blockColors[lastone->type % 4];
@@ -113,10 +130,11 @@ int movedown(struct coord* lastone){
 }
 
 int moveleft(struct coord* lastone){
-    if(shapesleftrightheight[lastone->type][0]+lastone->X<=0) return -1;
+    usleep(50000);
+    if(lastone->edges[0]+lastone->X<=0) return -1;
     for(int i=0;i<4;i++){
         for(int j=0;j<4;j++){
-            if(shapes[lastone->type][i][j]==1){
+            if(lastone->shapeofit[i][j]==1){
                 if(grid[lastone->Y+i][lastone->X+j-1]=='#') return -1;
                 else break;
             }
@@ -124,8 +142,8 @@ int moveleft(struct coord* lastone){
     }
     for(int i=0;i<4;i++){
         for(int j=0;j<4;j++){
-            if(shapes[lastone->type][i][j]==1){
-                if(shapes[lastone->type][i][j+1]!=1 || j+1>WIDTH){
+            if(lastone->shapeofit[i][j]==1){
+                if(lastone->shapeofit[i][j+1]!=1 || j+1>WIDTH){
                     grid[lastone->Y+i][lastone->X+j]=' ';
                 }
                 grid[lastone->Y+i][lastone->X+j-1]=blockColors[lastone->type % 4];
@@ -136,10 +154,11 @@ int moveleft(struct coord* lastone){
 }
 
 int moveright(struct coord* lastone){
-    if(shapesleftrightheight[lastone->type][1]+lastone->X>=WIDTH) return -1;
+    usleep(50000);
+    if(lastone->edges[1]+lastone->X>=WIDTH) return -1;
     for(int i=3;i>=0;i--){
         for(int j=3;j>=0;j--){
-            if(shapes[lastone->type][i][j]==1){
+            if(lastone->shapeofit[i][j]==1){
                 if(grid[lastone->Y+i][lastone->X+j+1]=='#') return -1;
                 else break;
             }
@@ -147,8 +166,8 @@ int moveright(struct coord* lastone){
     }
     for(int i=0;i<4;i++){
         for(int j=0;j<4;j++){
-            if(shapes[lastone->type][i][j]==1){
-                if(shapes[lastone->type][i][j-1]!=1 || j-1<0){
+            if(lastone->shapeofit[i][j]==1){
+                if(lastone->shapeofit[i][j-1]!=1 || j-1<0){
                     grid[lastone->Y+i][lastone->X+j]=' ';
                 }
                 grid[lastone->Y+i][lastone->X+j+1]=blockColors[lastone->type % 4];
@@ -158,9 +177,97 @@ int moveright(struct coord* lastone){
     lastone->X++;
 }
 
+void movelinesdown(int height){
+    for(int i=height;i>0;i--){
+        for (int j=0;j<WIDTH;j++){
+            grid[i][j]=grid[i-1][j];
+        }
+    }
+}
+
+void checkforlines(){
+    int wrong=0,nonefound=0;
+    while(!nonefound){
+        nonefound=1;
+        for (int i=HEIGHT-1;i>=0;i--){
+            wrong=0;
+            for(int j=0;j<WIDTH;j++){
+                if(grid[i][j]!='#'){
+                    wrong=1;
+                    break;
+                }
+            }
+            if(!wrong){
+                movelinesdown(i);
+                nonefound=0;
+                totalpoints+=10;
+                break;
+            }
+        }
+    }
+}
+
+void rotate(struct coord* lastone) {
+    usleep(30000);
+    int extra[4][4] = {0}; 
+    int left=4, right=-1, down=-1;
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j++) {
+            if(lastone->shapeofit[i][j] == 1) {
+                grid[lastone->Y + i][lastone->X + j] = ' ';
+            }
+        }
+    }
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j++) {
+            extra[j][i] = lastone->shapeofit[i][j];
+        }
+    }
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j++) {
+            if(extra[i][j] == 1) {
+
+                if(lastone->X + j >= WIDTH || lastone->X + j < 0 || 
+                   lastone->Y + i >= HEIGHT || 
+                   grid[lastone->Y + i][lastone->X + j] == '#') {
+                    for(int y = 0; y < 4; y++) {
+                        for(int x = 0; x < 4; x++) {
+                            if(lastone->shapeofit[y][x] == 1) {
+                                grid[lastone->Y + y][lastone->X + x] = blockColors[lastone->type % 4];
+                            }
+                        }
+                    }
+                    return;
+                }
+            }
+        }
+    }
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j++) {
+            lastone->shapeofit[i][j] = extra[i][j];
+            if(extra[i][j] == 1) {
+                if(j < left) left = j;
+                if(j > right) right = j;
+                if(i > down) down = i+1;
+            }
+        }
+    }
+    lastone->edges[0] = left;
+    lastone->edges[1] = right;
+    lastone->edges[2] = down;
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j++) {
+            if(lastone->shapeofit[i][j] == 1) {
+                grid[lastone->Y + i][lastone->X + j] = blockColors[lastone->type % 4];
+            }
+        }
+    }
+}
+
 extern int set_nonblocking();
 extern char setup_and_read();
 extern int init_terminal();
+extern int restore_terminal();
 
 int main() {
     int gameactive=1,hitground=0;
@@ -171,11 +278,23 @@ int main() {
     init_terminal();
     while(gameactive){
         srand(time(NULL));
-        int randomTetromino = rand() % 7;
-        lastone.X=0;
+        int randomTetromino = rand() % 10;
+        lastone.X=3;
         lastone.Y=0;
         lastone.type=randomTetromino;
-        placeshapes(randomTetromino); 
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                lastone.shapeofit[i][j]=shapes[randomTetromino][i][j];
+            }
+        }
+        for(int i=0;i<3;i++){
+            lastone.edges[i]=shapesleftrightheight[randomTetromino][i];
+        }
+
+        if(placeshapes(randomTetromino)==-1){
+            gameactive=0;
+            break;
+        }
         displayboard();
         hitground=0;
 
@@ -193,8 +312,21 @@ int main() {
             else if(tolower(current)=='a'){
                 moveleft(&lastone);
             }
+            else if(tolower(current)=='r'){
+                rotate(&lastone);
+            }
+            else if(current==3){ //Ctrl+C
+                return 0;
+            }
             displayboard();
         }
+        if(lastone.Y=0){
+            gameactive=0;
+            break;
+        }
+        checkforlines();
     }
+    restore_terminal();
+    printf("lil bro learn how to play!");
     return 0;
 }
--- rawchar.s
+++ rawchar.s
@@ -1,178 +1,233 @@
 section .data
-    STDIN       equ 0
-    STDOUT      equ 1
-    F_GETFL     equ 3
-    F_SETFL     equ 4
-    O_NONBLOCK  equ 0x800
-    FD_ZERO     equ 0
-    FD_SET      equ 1
-    TCGETS      equ 0x5401    ; ioctl code for getting terminal settings
-    TCSETS      equ 0x5402    ; ioctl code for setting terminal settings
-    ICANON      equ 0x0000002 ; canonical mode flag
-    ECHO        equ 0x0000008 ; echo flag
+    STDIN equ 0
+    STDOUT equ 1
+    F_GETFL equ 3
+    F_SETFL equ 4
+    O_NONBLOCK equ 0x800
+    FD_ZERO equ 0
+    FD_SET equ 1
+    TCGETS equ 0x5401    ; ioctl code for getting terminal settings
+    TCSETS equ 0x5402    ; ioctl code for setting terminal settings
+    ICANON equ 0x0000002 ; canonical mode flag
+    ECHO equ 0x0000008   ; echo flag
 
 section .bss
-    char            resb 1      
-    fd_set          resb 128    
-    termios:                    
-        c_iflag     resd 1      
-        c_oflag     resd 1      
-        c_cflag     resd 1      
-        c_lflag     resd 1      
-        c_line      resb 1      
-        c_cc        resb 19     
-        pad         resb 4      
-    orig_termios:               
-        resb 44                 
-    timeval:                    
-        tv_sec      resd 1
-        tv_usec     resd 1
+    char resb 1
+    fd_set resb 128
+    termios:
+        c_iflag resd 1
+        c_oflag resd 1
+        c_cflag resd 1
+        c_lflag resd 1
+        c_line resb 1
+        c_cc resb 19
+        pad resb 4
+    orig_termios:
+        resb 44
+    timeval:
+        tv_sec resd 1
+        tv_usec resd 1
 
 section .text
-global set_nonblocking
-global setup_and_read
-global restore_flags
-global init_terminal
-global restore_terminal
+    global init_terminal      ; Note the underscore prefix for C compatibility
+    global restore_terminal
+    global set_nonblocking
+    global setup_and_read
 
-; New function: Initialize terminal settings
-; Output: eax = 0 on success, -1 on error
+; Initialize terminal settings
+; C prototype: int init_terminal(void);
 init_terminal:
     push ebp
     mov ebp, esp
-    
+    push ebx                ; Save registers that cdecl requires to be preserved
+    push esi
+    push edi
+
     ; Get current terminal settings
-    mov eax, 54               ; sys_ioctl
+    mov eax, 54            ; sys_ioctl
     mov ebx, STDIN
     mov ecx, TCGETS
     mov edx, termios
     int 0x80
-    
+
     ; Check for error
     cmp eax, -1
     je .error
-    
+
     ; Save original settings
     push ecx
-    mov ecx, 44              ; size of termios struct
+    mov ecx, 44            ; size of termios struct
     mov esi, termios
     mov edi, orig_termios
     rep movsb
     pop ecx
-    
-    ; Modify flags to disable canonical mode and echo
-    mov eax, [termios + 12]  ; c_lflag
+
+    ; Modify flags
+    mov eax, [termios + 12] ; c_lflag
     and eax, ~(ICANON | ECHO)
     mov [termios + 12], eax
-    
-    ; Set minimum characters for non-canonical read
-    mov byte [termios + 17], 1    ; VMIN = 1
-    mov byte [termios + 18], 0    ; VTIME = 0
-    
-    ; Set new terminal settings
-    mov eax, 54              ; sys_ioctl
+
+    ; Set VMIN and VTIME
+    mov byte [termios + 17], 1  ; VMIN = 1
+    mov byte [termios + 18], 0  ; VTIME = 0
+
+    ; Apply new settings
+    mov eax, 54            ; sys_ioctl
     mov ebx, STDIN
     mov ecx, TCSETS
     mov edx, termios
     int 0x80
-    
+
     ; Check for error
     cmp eax, -1
     je .error
-    
-    xor eax, eax            ; Return 0
+
+    xor eax, eax           ; Return 0 on success
     jmp .exit
-    
+
 .error:
-    mov eax, -1
+    mov eax, -1            ; Return -1 on error
+
 .exit:
+    pop edi                ; Restore saved registers
+    pop esi
+    pop ebx
     mov esp, ebp
     pop ebp
     ret
 
-
-; set_nonblocking
-set_nonblocking:
+; Restore terminal settings
+; C prototype: int restore_terminal(void);
+restore_terminal:
     push ebp
     mov ebp, esp
-    ; Get current flags
-    mov eax, 221            ; sys_fcntl
+    push ebx               ; Save registers
+
+    ; Restore original settings
+    mov eax, 54           ; sys_ioctl
     mov ebx, STDIN
-    mov ecx, F_GETFL
+    mov ecx, TCSETS
+    mov edx, orig_termios
     int 0x80
+
     ; Check for error
     cmp eax, -1
     je .error
+
+    xor eax, eax          ; Return 0 on success
+    jmp .exit
+
+.error:
+    mov eax, -1           ; Return -1 on error
+
+.exit:
+    pop ebx               ; Restore saved registers
+    mov esp, ebp
+    pop ebp
+    ret
+
+; Set non-blocking mode
+; C prototype: int set_nonblocking(void);
+set_nonblocking:
+    push ebp
+    mov ebp, esp
+    push ebx              ; Save registers
+
+    ; Get current flags
+    mov eax, 221         ; sys_fcntl
+    mov ebx, STDIN
+    mov ecx, F_GETFL
+    int 0x80
+
+    ; Check for error
+    cmp eax, -1
+    je .error
+
     ; Save original flags
     push eax
+
     ; Set non-blocking mode
     or eax, O_NONBLOCK
     mov ecx, F_SETFL
     int 0x80
+
     ; Return original flags
     pop eax
     jmp .exit
+
 .error:
     mov eax, -1
+
 .exit:
+    pop ebx              ; Restore saved registers
     mov esp, ebp
     pop ebp
     ret
 
-;setup_and_read 
+; Read character with timeout
+; C prototype: int setup_and_read(void);
 setup_and_read:
     push ebp
     mov ebp, esp
+    push ebx             ; Save registers
+    push esi
+    push edi
 
-    ; Initialize fd_set (clear all bits)
+    ; Clear fd_set
     xor eax, eax
-    mov ecx, 32             ; 128 bytes = 32 dwords
+    mov ecx, 32         ; 128 bytes = 32 dwords
     mov edi, fd_set
-    rep stosd               ; Clear fd_set
+    rep stosd
 
-    ; Set stdin bit in fd_set
-    bts dword [fd_set], 0   ; Set bit 0 (stdin)
+    ; Set stdin bit
+    bts dword [fd_set], 0
 
+    ; Set timeout
     mov dword [tv_sec], 0
-    mov dword [tv_usec], 100000   ; 0.1 seconds
+    mov dword [tv_usec], 100000  ; 0.1 seconds
 
-    ; Call select()
-    mov eax, 142            ; sys_select
-    mov ebx, 1              ; nfds (check up to fd 1)
-    mov ecx, fd_set         ; read fds
-    xor edx, edx            ; write fds (NULL)
-    xor esi, esi            ; error fds (NULL)
-    mov edi, timeval        ; timeout
+    ; Call select
+    mov eax, 142        ; sys_select
+    mov ebx, 1          ; nfds
+    mov ecx, fd_set     ; read fds
+    xor edx, edx        ; write fds (NULL)
+    xor esi, esi        ; error fds (NULL)
+    mov edi, timeval    ; timeout
     int 0x80
 
     ; Check select result
     cmp eax, 0
-    je .timeout             
+    je .timeout
     cmp eax, -1
-    je .error              
+    je .error
 
-    mov eax, 3             ; sys_read
+    ; Read character
+    mov eax, 3         ; sys_read
     mov ebx, STDIN
     mov ecx, char
     mov edx, 1
     int 0x80
 
-    cmp eax, 1             ; Check if we read exactly one byte
+    cmp eax, 1         ; Check bytes read
     jne .error
 
-    ; Return the character
+    ; Return character
     movzx eax, byte [char]
     jmp .exit
 
 .timeout:
-    mov eax, -1           
+    mov eax, -1
     jmp .exit
 
 .error:
-    mov eax, -1           
+    mov eax, -1
 
 .exit:
+    pop edi            ; Restore saved registers
+    pop esi
+    pop ebx
     mov esp, ebp
     pop ebp
     ret
-section .note.GNU-stack
+
+section .note.GNU-stack noexec
